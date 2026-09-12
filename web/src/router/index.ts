ffaa79a4eee50dab4fc80b1d72180583ffa1cd60
jishengyang-cal <245595910+jishengyang-cal@users.noreply.github.com>
2026-09-12 20:38:41 +0000

    Remove retired browser credentials and restore compatible frontend builds

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -7,7 +7,6 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/Home.vue'),
     meta: {
       keepAlive: true, //此页面需要缓存
-      requiresFrontEndAuth: true,
       footerBg: "#F6FAFF"
     },
   },
@@ -17,7 +16,6 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/Playground.vue'),
     meta: {
       keepAlive: false, //此页面需要缓存
-      requiresFrontEndAuth: true,
       footerBg: "#fff"
     },
   },
@@ -27,42 +25,14 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/PlaygroundPage.vue'),
     meta: {
       keepAlive: false, //此页面需要缓存
-      requiresFrontEndAuth: true,
       footerBg: "#fff"
     },
   }
-  // {
-  //   path: '/Login',
-  //   name: 'Login',
-  //   component: () => import('../views/Login.vue'),
-  //   meta: {
-  //     keepAlive: false, //此页面需要缓存
-  //     requiresFrontEndAuth: false
-  //   },
-  // }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
-
-// 前端添加密码，防止release流程未走完，外部人员访问
-// router.beforeEach((to, from, next) => {
-//   console.log(from)
-//     if (!!to.meta && to.meta.requiresFrontEndAuth === false) {
-//         //这里判断用户是否登录，验证本地存储是否有token
-//         next();
-//         return;
-//     }
-//     if (!sessionStorage.getItem("token")) { // 判断当前的token是否存在
-//         next({
-//             name: 'Login',
-//             query: { redirect: to.fullPath }
-//         })
-//     } else {
-//         next();
-//     }
-// })
 
 export default router
